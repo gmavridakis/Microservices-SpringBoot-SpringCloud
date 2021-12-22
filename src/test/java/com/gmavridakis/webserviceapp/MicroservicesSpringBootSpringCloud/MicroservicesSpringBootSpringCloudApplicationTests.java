1e4c0b5eb7e783b853afd06d13c5cc0ff7c9f5f1
gmavridakis <gmavridakis@scorpiogroup.net>
2021-12-22 13:26:34 +0200

    setup commit with users controller

---
+++ src/test/java/com/gmavridakis/webserviceapp/MicroservicesSpringBootSpringCloud/MicroservicesSpringBootSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmavridakis.webserviceapp.MicroservicesSpringBootSpringCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesSpringBootSpringCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
